bb17da494224ba2dfda37d390d494f6bfbed6101
Shlomo M <shlomac@macmini.myfiosgateway.com>
2026-04-15 00:30:59 -0400

    iOS App

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,22 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    if let controller = window?.rootViewController as? FlutterViewController {
+      let appChannel = FlutterMethodChannel(name: "com.mindq/app", binaryMessenger: controller.binaryMessenger)
+      appChannel.setMethodCallHandler { (call, result) in
+        if call.method == "moveToBackground" {
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            UIApplication.shared.perform(NSSelectorFromString("suspend"))
+          }
+          result(nil)
+        } else {
+          result(FlutterMethodNotImplemented)
+        }
+      }
+    }
+
+    return result
   }
 }
